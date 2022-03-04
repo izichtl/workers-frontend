1633a1b497b5913f7a927e2264b8ac4989b2411a
iZichtl <izichtl@gmail.com>
2022-03-04 15:54:58 -0300

    install and config of MaterialUI, home, login, signup and dashboard template page install

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,58 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-// import MainNavbar from '../components/Navbars/MainNavbar';
-// import Main from '../components/Navbars/MainLayout';
-// import NavBar from '../components/Navbar';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+// import Box from '@mui/material/Box';
+import {
+  Box,
+  Typography,
+  Container,
+  Button,
+} from '@mui/material';
+// import Container from '@mui/material/Container';
+import Copyright from '../components/Copyright';
 
-const Home = () => (
-  <>
-    <div>
-      HOME AQUI
-    </div>
-  </>
-);
 
-export default Home;
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <CssBaseline />
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Maxxi Workers
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {'Dashboard para controle e cadastro de Profissionais'}
+          <br />
+        </Typography>
+        <Typography variant="body">Aplicação para teste de ingresso, desenvolvido utilizando React/MaterialUI.</Typography>
+        <br />
+        <Button variant="contained">Login</Button>
+      </Container>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            Desenvolvido com carinho por, Ivan Zichtl
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/Home2.js
+++ src/pages/Home2.js
@@ -1,15 +1,7 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-// import MainNavbar from '../components/Navbars/MainNavbar';
-// import Main from '../components/Navbars/MainLayout';
-// import NavBar from '../components/Navbar';
-
-const Home2 = () => (
-  <>
-    <div>
-      HOME AQUI2
-    </div>
-  </>
-);
-
-export default Home2;
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Footer from '../components/Footer'
+function ButtonT() {
+  return <Button variant="contained"> Olá Mundo</Button>;
+} 
+export default ButtonT;
